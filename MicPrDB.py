16a8659e0027728bf0e6ded263c1738e30e79fb7
Somi Afiuni <somi.afuniz@gmail.com>
2017-03-30 11:19:18 -0400

    Add files via upload

---
+++ MicPrDB.py
@@ -0,0 +1,182 @@
+mic_list=[
+'Lactobacillus crispatus MV-3A-US',
+'Lactobacillus gasseri 202-4',
+'Lactobacillus iners UPII 60-B',
+'Megasphaera sp. UPII 199-6',
+'Mobiluncus mulieris ATCC 35243',
+'Lactobacillus iners LEAF 2052A-d',
+'Lactobacillus gasseri SV-16A-US',
+'Streptococcus agalactiae BV3L5',
+'Enterococcus faecalis EnGen0311',
+'Escherichia coli 83972',
+'Gardnerella vaginalis 409-05',
+'Lactobacillus oris PB013-T2-3',
+'Actinomyces turicensis ACS-279-V-Col4',
+'Lactobacillus crispatus JV-V01',
+'Finegoldia magna ACS-171-V-Col3',
+'Propionimicrobium sp. BV2F7',
+'Lactobacillus iners LactinV 03V1-b',
+'Atopobium vaginae PB189-T1-4',
+'Mobiluncus curtisii ATCC 43063',
+'Chryseobacterium gleum ATCC 35910',
+'Corynebacterium glucuronolyticum ATCC 51866',
+'Enterococcus faecium FB129-CNAB4',
+'Staphylococcus lugdunensis ACS-027-V-Sch2',
+'Acidaminococcus sp. BV3L6',
+'Lactobacillus crispatus FB077-07',
+'Lactobacillus jensenii 1153',
+'Dialister micraerophilus DSM 19965',
+'Prevotella buccalis ATCC 35310',
+'Prevotella sp. BV3P1',
+'Peptoniphilus sp. BV3AC2',
+'Lactobacillus iners SPIN 2503V10-D',
+'Aerococcus urinae ACS-120-V-Col10a',
+'Streptococcus urinalis FB127-CNA-2',
+'Propionibacterium sp. 409-HC1',
+'Lactobacillus crispatus CTV-05',
+'Lactobacillus iners LEAF 3008A-a',
+'Lactobacillus iners LactinV 11V1-d',
+'Anaerococcus lactolyticus ATCC 51172',
+'Prevotella timonensis CRIS 5C-B1',
+'Proteus mirabilis WGLW4',
+'Corynebacterium pseudogenitalium ATCC 33035',
+'Mobiluncus mulieris ATCC 35239',
+'Enterococcus faecalis EnGen0297',
+'Corynebacterium aurimucosum ATCC 700975',
+'Acinetobacter baumannii ATCC 19606 = CIP 70.34',
+'Lactobacillus fermentum 28-3-CHN',
+'Megasphaera sp. BV3C16-1',
+'Lactobacillus jensenii 115-3-CHN',
+'Corynebacterium glucuronolyticum ATCC 51867',
+'Peptoniphilus harei ACS-146-V-Sch2b',
+'Haemophilus parainfluenzae HK262',
+'Actinomyces europaeus ACS-120-V-Col10b',
+'Lactobacillus iners SPIN 1401G',
+'Dialister microaerophilus UPII 345-E',
+'Streptococcus equinus ATCC 700338',
+'Porphyromonas uenonis 60-3',
+'Finegoldia magna BVS033A4',
+'Propionimicrobium lymphophilum ACS-093-V-SCH5',
+'Atopobium sp. BV3Ac4',
+'Sphingobacterium spiritivorum ATCC 33300',
+'Lactobacillus crispatus SJ-3C-US',
+'Enterococcus faecalis ATCC 29200',
+'Mobiluncus curtisii subsp. holmesii ATCC 35242',
+'Lactobacillus iners UPII 143-D',
+'Mobiluncus mulieris 28-1',
+'Enterococcus faecalis TX0312',
+'Atopobium vaginae DSM 15829',
+'Corynebacterium jeikeium ATCC 43734',
+'Corynebacterium genitalium ATCC 33030',
+'Actinomyces urogenitalis DSM 15434',
+'Lactobacillus crispatus FB049-03',
+'Klebsiella pneumoniae subsp. pneumoniae WGLW1',
+'Mobiluncus curtisii ATCC 51333',
+'Lactobacillus crispatus 125-2-CHN',
+'Lactobacillus crispatus MV-1A-US',
+'Lactobacillus iners LactinV 01V1-a',
+'Clostridiales genomosp. BVAB3 str. UPII9-5',
+'Lactobacillus iners LEAF 2062A-h1',
+'Fusobacterium nucleatum subsp. nucleatum ATCC 23726',
+'Mobiluncus curtisii subsp. curtisii ATCC 35241',
+'Actinobaculum massiliae ACS-171-V-Col2',
+'Brevibacterium mcbrellneri ATCC 49030',
+'Gardnerella vaginalis ATCC 14019',
+'Lactobacillus vaginalis ATCC 49540',
+'Prevotella denticola CRIS 18C-A',
+'Campylobacter ureolyticus ACS-301-V-Sch3b',
+'Enterococcus faecalis TX0855',
+'Anaerococcus hydrogenalis ACS-025-V-Sch4',
+'Coriobacteriaceae bacterium BV3Ac1',
+'Anaerococcus tetradius ATCC 35098',
+'Lactobacillus iners LEAF 2053A-b',
+'Lactobacillus gasseri MV-22',
+'Facklamia hominis ACS-120-V-Sch10',
+'Peptoniphilus lacrimalis 315-B',
+'Lactobacillus coleohominis 101-4-CHN',
+'Mobiluncus mulieris FB024-16',
+'Actinomyces coleocanis DSM 15436',
+'Finegoldia magna SY403409CC001050417',
+'Eremococcus coleocola ACS-139-V-Col8',
+'Bifidobacterium dentium JCVIHMP022',
+'Lactobacillus jensenii 27-2-CHN',
+'Lactobacillus gasseri JV-V03',
+'Staphylococcus simulans ACS-120-V-Sch1',
+'Veillonella ratti ACS-216-V-Col6b',
+'Megasphaera genomosp. type_1 str. 28L',
+'Lactobacillus johnsonii ATCC 33200',
+'Mycobacterium parascrofulaceum ATCC BAA-614',
+'Veillonella atypica ACS-049-V-Sch6',
+'Lactobacillus iners ATCC 55195',
+'Staphylococcus epidermidis BVS058A4',
+'Aerococcus viridans ATCC 11563',
+'Lactobacillus gasseri 224-1',
+'Peptoniphilus sp. BV3C26',
+'Lactobacillus iners LactinV 09V1-c',
+'Veillonella parvula ACS-068-V-Sch12',
+'Bifidobacterium dentium ATCC 27679',
+'Peptoniphilus duerdenii ATCC BAA-1640',
+'Lactobacillus delbrueckii subsp. bulgaricus PB2003/044-T3-4',
+'Lactobacillus jensenii 269-3',
+'Proteus mirabilis ATCC 29906',
+'Prevotella bivia JCVIHMP010',
+'Porphyromonas asaccharolytica PR426713P-I',
+'Prevotella oralis ATCC 33269',
+'Roseomonas cervicalis ATCC 49957',
+'Finegoldia magna ATCC 53516',
+'Jonquetella sp. BV3C21',
+'Lactobacillus iners DSM 13335',
+'Propionibacterium sp. 434-HC2',
+'Anaerococcus prevotii ACS-065-V-Col13',
+'Streptococcus pseudoporcinus SPIN 20026',
+'Lactobacillus jensenii SJ-7A-US',
+'Lactobacillus salivarius ACS-116-V-Col5a',
+'Corynebacterium lipophiloflavum DSM 44291',
+'Bifidobacterium breve ACS-071-V-Sch8b',
+'Lactobacillus crispatus 214-1',
+'Prevotella disiens FB035-09AN',
+'Lactobacillus gasseri SJ-9E-US',
+'Gardnerella vaginalis 315-A',
+'Corynebacterium striatum ATCC 6940',
+'Prevotella amnii CRIS 21A-A',
+'Actinomyces neuii BVS029A5',
+'Gardnerella vaginalis HMP9231',
+'Treponema phagedenis F0421',
+'Lactobacillus jensenii JV-V16',
+'Sphingobacterium spiritivorum ATCC 33861',
+'Actinobaculum schaalii FB123-CNA-2',
+'Staphylococcus aureus subsp. aureus MN8'
+    ]
+import urllib,urllib2
+print 'GENERATING DATABASE...'  
+a=[]
+res = ""
+for mic in mic_list:
+    url = 'http://www.uniprot.org/uniprot/'
+    params = {
+    'format':'fasta',
+    'query':mic
+    }
+    data = urllib.urlencode(params)
+    request = urllib2.Request(url, data)
+    response = urllib2.urlopen(request)
+    content = response.read()
+    
+    if len(content.split())==0:
+        a.append(mic)
+        with open('C:\Users\BLT.Project\Desktop\MicPrDB-PC\NoPrSeq.fasta', 'w') as output:
+            output.write('\n'.join(a))
+    else:
+        with open('C:\Users\BLT.Project\Desktop\MicPrDB-PC\PrSeq.fasta', 'a') as result:
+            result.write(content)
+
+result.closed
+print '\n...DONE!!!'
+
+
+
+
+    
+
+
+
